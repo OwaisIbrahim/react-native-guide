ad30f5a88d37f254e89431116c6f4ba69d6af653
Owais Ibrahim <owaisibrahim99@gmail.com>
2020-02-23 22:52:11 +0500

    Initial Commit

---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const person = () => {
+    return <p>I am a person</p>
+}
+
+// as person is default we can import it by any alias example p1, Person
+export default person;
